fc93a57513afdda4a1e438aaf4fe5a57f7a1a896
Javokhir Kubaev <jkubaev7@gmail.com>
2026-04-02 15:15:01 +0200

    fstreams

---
+++ tutorial/CPP01/fstream/sample.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+	std::ifstream	ifs("numbers");
+	unsigned	int	dst;
+	unsigned	int	dst2;
+	ifs >> dst >> dst2;
+
+	std::cout << dst << " " << dst2 << std::endl;
+	ifs.close();
+
+	std::ofstream	ofs("test.out");
+	ofs << "i like panies wholle a lot damm" << std::endl;
+	ofs.close();
+	return (0);
+}
