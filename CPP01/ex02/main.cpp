6b334d2b1211b3c3bee496c39421352ee68ed4ee
Javokhir Kubaev <jkubaev7@gmail.com>
2026-04-03 09:49:56 +0200

    Completed ex02

---
+++ CPP01/ex02/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string	str;
+	std::string	*stringPTR;
+	std::string &stringREF = str;
+
+	str = "HI THIS IS BRAIN";
+
+	std::cout << "The memory address of the string variable: " << &str << std::endl;
+	std::cout << "The memory address held by stringPTR: " << &stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+	
+	std::cout << "The value of the string variable: " << str << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+	return (0);
+}
