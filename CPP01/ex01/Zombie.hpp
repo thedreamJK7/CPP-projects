143f11d264e1768dbf218151c5430da2b02012ba
Javokhir Kubaev Macbook <jkubaev7@gmail.com>
2026-04-03 06:45:29 +0200

    ex03

---
+++ CPP01/ex01/Zombie.hpp
@@ -0,0 +1,21 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <iostream>
+#include <string>
+
+class Zombie
+{
+	private:
+		std::string	_name;
+
+	public:
+		Zombie(const std::string &name);
+		~Zombie();
+
+		void		announce( void );
+};
+
+Zombie* zombieHorde( int N, std::string name );
+
+#endif
---
+++ CPP01/ex01/zombieHorde.cpp
@@ -0,0 +1,11 @@
+#include "Zombie.hpp"
+
+Zombie* zombieHorde( int N, std::string name )
+{
+	Zombie (*zombie)[10];
+
+	for (int i = 0; i < N; i++)
+	{
+			
+	}
+}
