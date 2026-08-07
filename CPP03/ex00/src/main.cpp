7f5d3b5d4f8ad4eceddd4a9df7684f9d10a03ef7
Javokhir Kubaev Macbook <jkubaev7@gmail.com>
2026-08-07 17:21:12 +0200

    tutorial of nested classes

--- CPP03/ex00/src/main.cpp
+++ CPP03/ex00/src/main.cpp
@@ -14,6 +14,7 @@ int main()
 
         a.beRepaired(5);
     }
+
     {
         ClapTrap	trapA("Hugo Boss");
         ClapTrap	trapB("John Dirty");
---
+++ tutorial/CPP05/NestedClasses/nested.hpp
@@ -0,0 +1,21 @@
+#ifndef NESTED_HPP
+#define NESTED_HPP
+
+class Cat 
+{
+	public:
+		class Leg
+		{
+
+		};
+};
+
+class Dog 
+{
+	public:
+		class Leg
+		{
+
+		};
+};
+#endif
