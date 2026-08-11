7e049e5de30b77ac304ab60a1505fb07bd6d29ae
Javokhir Kubaev Macbook <jkubaev7@gmail.com>
2026-08-11 16:21:14 +0200

    lets work on linux computer

---
+++ CPP05/ex02/src/AForm.cpp
@@ -0,0 +1,92 @@
+#include "../includes/AForm.hpp"
+
+// Exceptions
+const char *AForm::GradeTooHighException::what() const throw()
+{
+	return	"Grade too HIGH:\n"
+			"it has to be higher than 0 and smaller/equal to 150!";
+}
+
+const char *AForm::GradeTooLowException::what() const throw()
+{
+	return	"Grade too LOW:\n"
+			"it has to be higher than 0 and smaller/equal to 150!";
+}
+
+//constructor
+AForm::AForm(const std::string & name, 
+		const int & gradeToSign, const int & gradeToExecute)
+: _name(name),
+_isSigned(false),
+_gradeToSign(gradeToSign), 
+_gradeToExecute(gradeToExecute)
+{
+	if (gradeToSign > 150)
+		throw AForm::GradeTooLowException();
+	else if (gradeToSign < 1)
+		throw AForm::GradeTooHighException();
+
+	if (gradeToExecute > 150)
+		throw AForm::GradeTooLowException();
+	else if (gradeToExecute < 1)
+		throw AForm::GradeTooHighException();
+}
+
+//copy constructor
+AForm::AForm(const AForm & rhs): 
+_name(rhs._name),
+_isSigned(rhs._isSigned),
+_gradeToSign(rhs._gradeToSign),
+_gradeToExecute(rhs._gradeToExecute)
+{ }
+
+// assignemt operator
+AForm & AForm::operator=(const AForm &rhs)
+{
+	_isSigned = rhs._isSigned;
+	return (*this);
+}
+
+// destructor
+AForm::~AForm() {  }
+
+// getters
+const std::string	&AForm::getName(void) const
+{
+	return(_name);
+}
+
+const int	&AForm::getGradeToSign(void) const
+{
+	return (_gradeToSign);
+}
+
+const int	&AForm::getGradeToExecute(void) const
+{
+	return (_gradeToExecute);
+}
+
+bool	AForm::getIsSigned(void)	const
+{
+	return (_isSigned);
+}
+
+// operator overload
+std::ostream	&operator<<(std::ostream &os, const AForm &Aform)
+{
+	os	<< "Name: "					<< Aform.getName ()
+		<< "\nSignature status: "	<< Aform.getIsSigned ()
+		<< "\nSignature grade: "	<< Aform.getGradeToSign ()
+		<< "\nExecution grade: "	<< Aform.getGradeToExecute ();
+	return (os);
+}
+
+// member functions
+void	AForm::beSigned(const Bureaucrat &bureaucrat)
+{
+	if (bureaucrat.getGrade() <= _gradeToSign)
+		_isSigned = true;
+	else
+		throw AForm::GradeTooLowException();
+}
+
---
+++ CPP05/ex02/src/Bureaucrat.cpp
@@ -0,0 +1,85 @@
+#include "../includes/Bureaucrat.hpp"
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return	"Grade too HIGH:\n"
+			"it has to be higher than 0 and smaller/equal to 150!";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return	"Grade too LOW:\n"
+			"it has to be higher than 0 and smaller/equal to 150!";
+}
+
+Bureaucrat::Bureaucrat(const std::string & name, int grade)
+: _name(name)
+{
+	if (grade > 150)
+		throw Bureaucrat::GradeTooLowException();
+	else if (grade < 1)
+		throw Bureaucrat::GradeTooHighException();
+	_grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat & rhs): _name(rhs._name), _grade(rhs._grade)
+{ }
+
+Bureaucrat & Bureaucrat::operator=(const Bureaucrat &rhs)
+{
+	_grade = rhs._grade;
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat() {  };
+
+const std::string &Bureaucrat::getName(void) const
+{
+	return (_name);
+}
+
+const int &Bureaucrat::getGrade(void) const
+{
+	return (_grade);
+}
+
+void Bureaucrat::decrementGrade(void)
+{
+	int temp = _grade + 1;
+
+	if (temp > 150)
+		throw Bureaucrat::GradeTooLowException();
+	_grade++;
+}
+
+void Bureaucrat::incrementGrade(void)
+{
+	int temp = _grade - 1;
+
+	if (temp < 1)
+		throw Bureaucrat::GradeTooHighException();
+	_grade--;
+}
+
+std::ostream	&operator<<(std::ostream &os, const Bureaucrat &member)
+{
+	os << member.getName() << ", bureaucrat grade " << member.getGrade();
+	return (os);
+}
+
+void	Bureaucrat::signForm(AForm &form)
+{
+	try
+	{
+		form.beSigned(*this);
+		std::cout	<< this->_name		<<	" signed "
+				<< form.getName ()	<< "."
+				<< std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout	<< _name		<<	" couldn’t sign "
+					<< form.getName ()	<< " because grade too low."
+					<< std::endl;	
+	}
+}
