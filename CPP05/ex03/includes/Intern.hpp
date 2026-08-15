63cf3b4d3e2c9010119c537579233e040aff9b3c
Javokhir Kubaev <jkubaev7@gmail.com>
2026-08-15 11:51:02 +0200

    implementation of ex03

--- CPP05/ex03/includes/Intern.hpp
+++ CPP05/ex03/includes/Intern.hpp
@@ -1,18 +1,30 @@
 #include "AForm.hpp"
 #include <string>
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 #ifndef Intern_H
 #define Intern_H
 
+class ShrubberyCreationForm;
+class RobotomyRequestForm;
+class PresidentialPardonForm;
 class Intern {
 
 	public:
-		Intern();                          // empty constructor
-		Intern(Intern const &);            // copy constructor
-		Intern &operator=(Intern const &); // copy assignment operator
+		Intern();                        // empty constructor
+		Intern(Intern const &rhs);            // copy constructor
+		Intern &operator=(Intern const &rhs); // copy assignment operator
 		~Intern();                         // destructor
 
-		AForm *makeForm(std::string formType, std::string target);
+		AForm	*makeForm(const std::string &form_name, 
+						const std::string &target_form);
+
+	private:
+		AForm	*shrubbery(const std::string &target_form);
+		AForm	*presidential(const std::string &target_form);
+		AForm	*request(const std::string &target_form);
 };
 
 #endif
