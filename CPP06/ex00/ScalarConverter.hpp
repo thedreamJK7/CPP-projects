b0b46a91cc74d7e6bf7dfb9637888de4b835f8bf
Javokhir Kubaev <jkubaev7@gmail.com>
2026-08-19 17:31:02 +0200

    implementation of cpp06 ex00

--- CPP06/ex00/ScalarConverter.hpp
+++ CPP06/ex00/ScalarConverter.hpp
@@ -1,15 +1,27 @@
 #ifndef SCALARCONVERTER_HPP
 #define SCALARCONVERTER_HPP
 
+#include <string>
+#include <iostream>
+#include <limits>
+#include <cctype>
+#include <cmath>
+#include <sstream>
+#include <iomanip>
+#include <cstdlib>
+
+
 class ScalarConverter
 {
-	public:
+	// no instantiation - private constructors
+	private:
 		ScalarConverter(); // default constructor
 		ScalarConverter(const ScalarConverter &rhs); // copy constructor
 		ScalarConverter & operator=(const ScalarConverter &rhs); // assignment operator
 		~ScalarConverter();
 
-
+	public:
+		static void	convert(const std::string &);
 };
 
 #endif
--- CPP06/ex00/main.cpp
+++ CPP06/ex00/main.cpp
@@ -3,6 +3,11 @@
 int main(int argc, char const *argv[])
 {
 	if (argc != 2)
+	{
+		std::cout	<< "Argument count error, hint: this program takes 1 arguments!"
+					<< std::endl;
 		return (1);
+	}
+	ScalarConverter::convert(argv[1]);
 	return (0);
 }
