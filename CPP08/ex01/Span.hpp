8a8b24817a00c77614758487f8e32d5af0ef6c38
Javokhir Kubaev <jkubaev7@gmail.com>
2026-08-29 00:05:07 +0200

    implementation of ex01

--- CPP08/ex01/Span.hpp
+++ CPP08/ex01/Span.hpp
@@ -1,16 +1,14 @@
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
+#include <iostream>
 #include <vector>
 #include <exception>
-class Span
-{
-	private:
-		std::vector<int>	_n;
-		unsigned int		_size;
-	
+#include <algorithm>
+
+class Span {	
 	public:
-		Span(unsigned int & n);
+		Span(const unsigned int n);
 		Span(Span const & rhs);
 		Span & operator = (Span const & rhs);
 		~Span();
@@ -19,9 +17,34 @@ class Span
 		class outOfRange: public std::exception
 		{
 			public:
-				virtual const char *what() const throw();
+				virtual const char *what() const throw() {
+					return "Out of range!";
+				}
+		};
+		class noSpan: public std::exception
+		{
+			public:
+				virtual const char *what() const throw() {
+					return "No span for items with elements'count less than 2";
+				}
 		};
 
+		// getter, setter
+		unsigned int	getSize() const { return _size; };
+		unsigned int	getCounter() const { return _counter; };
+		void			printAll(std::vector<int> &data) const {
+			for (size_t i = 0; i < _counter; i++)
+				std::cout << data[i] << " ";
+			std::cout << std::endl;
+		};
+
+		void			addNumber(int n);
+		unsigned int	shortestSpan(void);
+		unsigned int	longestSpan(void);
+	private:
+		std::vector<int>	_n;
+		unsigned int		_size;
+		unsigned int		_counter;
 };
 
 #endif
